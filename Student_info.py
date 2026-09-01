046ab9d27edd3ec92a4ec4fbb08a951b313b28df
Koshal-ai <parialkoshal357@gmail.com>
2026-09-01 01:18:05 -0700

    Create Student_info.py

---
+++ Student_info.py
@@ -0,0 +1,13 @@
+student = {
+    "name": "Koshal",
+    "age": 18,
+    "course": "BSc AI"
+}
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
+
+student["skill"] = "Python"
+
+print("Updated Student:", student)
